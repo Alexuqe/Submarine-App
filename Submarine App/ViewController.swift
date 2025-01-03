280e92ad282a96f8a0bbbdf55ef522dadfc84b9d
alex <hordiah1@google.com>
2025-01-03 15:34:09 +0300

    Initial Commit

---
+++ Submarine App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Submarine App
+//
+//  Created by Sasha on 3.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
